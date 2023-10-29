cf548f1958dcb1f5fe11986ab6e8aec9482ace29
young ho moon <ansdudgh98@naver.com>
2023-10-30 01:51:57 +0900

    feat : "cookie 이름을 상수값으로 생성 "

---
+++ backend/src/main/java/com/cherish/backend/controller/ConstValue.java
@@ -0,0 +1,8 @@
+package com.cherish.backend.controller;
+
+public interface ConstValue {
+
+    final static String tokenCookieName = "sessiontoken";
+    final static String sessionName = "avatarId";
+
+}

21672a5b5d09ec9129212bee862b1042a62a9462
young ho moon <ansdudgh98@naver.com>
2023-11-06 21:44:29 +0900

    feat : "diary 종류 enum Type converter 추가"

--- backend/src/main/java/com/cherish/backend/domain/DiaryKind.java
+++ backend/src/main/java/com/cherish/backend/domain/DiaryKind.java
@@ -1,9 +1,31 @@
 package com.cherish.backend.domain;
 
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+import java.util.stream.Stream;
+
 public enum DiaryKind {
 
-    EMOTION,
-    QUESTION,
-    FREE
+    EMOTION("감정형식"),
+    QUESTION("질문형식"),
+    FREE("자유형식");
+
+    String value;
+
+    DiaryKind(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    @JsonCreator
+    public static DiaryKind parsing(String inputValue) {
+        return Stream.of(DiaryKind.values())
+                .filter(diaryKind -> diaryKind.getValue().equals(inputValue))
+                .findFirst()
+                .orElse(null);
+    }
 }

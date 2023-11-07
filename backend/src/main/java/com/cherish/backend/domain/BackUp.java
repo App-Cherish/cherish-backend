ac6a40dc315aef98e661a6294a63be9a7465f255
young ho moon <ansdudgh98@naver.com>
2023-11-07 18:52:58 +0900

    feat : "BackUp Entity 수정" #6

--- backend/src/main/java/com/cherish/backend/domain/BackUp.java
+++ backend/src/main/java/com/cherish/backend/domain/BackUp.java
@@ -1,8 +1,6 @@
 package com.cherish.backend.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -28,18 +26,25 @@ public class BackUp {
     @Column(name = "created_date",updatable = false)
     private LocalDateTime createdDate;
 
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "avatar_id")
+    private Avatar avatar;
+
     @Builder
-    private BackUp(String id, String osVersion, String deviceType, int diaryCount) {
+    private BackUp(String id, String osVersion, String deviceType, int diaryCount, Avatar avatar) {
         this.id = id;
         this.osVersion = osVersion;
         this.deviceType = deviceType;
         this.diaryCount = diaryCount;
         this.createdDate = LocalDateTime.now();
+        this.avatar = avatar;
     }
 
-    public static BackUp of(String id,String osVersion, String deviceType, int diaryCount) {
+
+    public static BackUp of(String id,String osVersion, String deviceType, int diaryCount,Avatar avatar) {
         return BackUp.builder()
                 .id(id)
+                .avatar(avatar)
                 .osVersion(osVersion)
                 .deviceType(deviceType)
                 .diaryCount(diaryCount)

64d771da6cdf5e9c6f4e7beae821be0cd884ec75
young ho moon <ansdudgh98@naver.com>
2023-11-16 22:53:42 +0900

    refactor : "docs 수정"

--- backend/src/test/java/com/cherish/backend/controller/docs/BackUpControllerDocs.java
+++ backend/src/test/java/com/cherish/backend/controller/docs/BackUpControllerDocs.java
@@ -1,12 +1,12 @@
 package com.cherish.backend.controller.docs;
 
 import com.cherish.backend.controller.ConstValue;
+import com.cherish.backend.domain.Avatar;
 import com.cherish.backend.domain.BackUp;
+import com.cherish.backend.domain.Gender;
 import com.cherish.backend.repositroy.AvatarRepository;
-import com.cherish.backend.service.BackUpService;
-import com.cherish.backend.service.DiaryService;
+import com.cherish.backend.repositroy.BackUpRepository;
 import com.epages.restdocs.apispec.ResourceSnippetParameters;
-import com.epages.restdocs.apispec.Schema;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -15,8 +15,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpSession;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
@@ -24,16 +22,14 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.time.LocalDate;
+
 import static com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper.document;
 import static com.epages.restdocs.apispec.ResourceDocumentation.resource;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.BDDMockito.given;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
-import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(RestDocumentationExtension.class)
@@ -50,11 +46,11 @@ public class BackUpControllerDocs {
 
     MockHttpSession session;
 
-    @MockBean
-    BackUpService backUpService;
-
     @Autowired
     AvatarRepository avatarRepository;
+
+    @Autowired
+    BackUpRepository backUpRepository;
 
     @BeforeEach
     void init(RestDocumentationContextProvider restDocumentationContextProvider) {
@@ -71,8 +67,10 @@ public class BackUpControllerDocs {
     @DisplayName("[DOCS]가장 최근 백업 불러오기")
     public void getRecentBackUpTest() throws Exception {
         //given
-        BackUp backUp = BackUp.of("newBackUpId1", "os1", "deviceType1", 3, null);
-        given(backUpService.getRecentBackUp(eq(1L))).willReturn(backUp);
+        Avatar avatar = Avatar.of("name1", LocalDate.now(), Gender.MALE);
+        avatarRepository.save(avatar);
+        BackUp backUp = BackUp.of("asdasd", "os1", "device1", 3, avatar);
+        backUpRepository.save(backUp);
 
         //when
         //then

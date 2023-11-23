a0b4267cf95c8b25a9e420a24f4d4c428893a6ac
young ho moon <ansdudgh98@naver.com>
2023-11-23 20:43:42 +0900

    remove : "안 쓰는 파일 삭제"

--- backend/src/main/java/com/cherish/backend/exception/ExistLoginHistoryException.java
+++
@@ -1,9 +0,0 @@
-package com.cherish.backend.exception;
-
-public class ExistLoginHistoryException extends RuntimeException {
-
-    public ExistLoginHistoryException() {
-        super("이번 기기에서 로그인 히스토리가 존재합니다. 타 플랫폼 API로 요청을 다시 보내주세요.");
-    }
-
-}

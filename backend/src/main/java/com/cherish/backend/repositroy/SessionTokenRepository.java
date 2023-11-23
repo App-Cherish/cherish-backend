c424392bd88a34b53b5086388ac90b122f4962ba
young ho moon <ansdudgh98@naver.com>
2023-11-23 20:18:23 +0900

    feat : "회원탈퇴 시에 활성화 되어있는 모든 토큰을 찾기 위한 조회용 쿼리 코드 작성" #17

--- backend/src/main/java/com/cherish/backend/repositroy/SessionTokenRepository.java
+++ backend/src/main/java/com/cherish/backend/repositroy/SessionTokenRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface SessionTokenRepository extends JpaRepository<SessionToken, Long> {
@@ -15,7 +16,7 @@ public interface SessionTokenRepository extends JpaRepository<SessionToken, Long
     @Query(value = "select s from session_token s where s.deviceId=:deviceId")
     Optional<SessionToken> findSessionTokenByDeviceId(@Param("deviceId") String deviceId);
 
-    @Query(value = "select count(s) > 0 from session_token s where s.deviceId=:deviceId")
-    Boolean existSessionTokenByDeviceId(@Param("deviceId") String deviceId);
+    @Query(value = "select s from session_token s where s.avatar.id = :avatarId and s.active=1")
+    List<SessionToken> findSessionTokenByAvatarId(@Param("avatarId") Long avatarId);
 
 }

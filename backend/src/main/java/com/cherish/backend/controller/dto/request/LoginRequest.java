2ae18498067a20e42f0e23e7711eb129c5bf475e
young ho moon <ansdudgh98@naver.com>
2023-11-23 20:17:38 +0900

    feat : "LoginDto 및 Request에 Platform 추가" #17

--- backend/src/main/java/com/cherish/backend/controller/dto/request/LoginRequest.java
+++ backend/src/main/java/com/cherish/backend/controller/dto/request/LoginRequest.java
@@ -8,11 +8,13 @@ import lombok.NoArgsConstructor;
 public class LoginRequest {
 
     private String oauthId;
+    private String platform;
     private String deviceId;
     private String deviceType;
 
-    public LoginRequest(String oauthId, String deviceId, String deviceType) {
+    public LoginRequest(String oauthId, String platform, String deviceId, String deviceType) {
         this.oauthId = oauthId;
+        this.platform = platform;
         this.deviceId = deviceId;
         this.deviceType = deviceType;
     }

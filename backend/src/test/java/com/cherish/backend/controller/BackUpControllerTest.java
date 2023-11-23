2a297ac4d183772510839c32531dfac81fd91ddb
young ho moon <ansdudgh98@naver.com>
2023-11-23 20:35:29 +0900

    test : "백업 기능에 대한 테스트 작성" #26

--- backend/src/test/java/com/cherish/backend/controller/BackUpControllerTest.java
+++ backend/src/test/java/com/cherish/backend/controller/BackUpControllerTest.java
@@ -16,19 +16,22 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-
 
 import java.time.LocalDate;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@Profile("dev")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @Transactional
 @AutoConfigureMockMvc
@@ -75,7 +78,7 @@ class BackUpControllerTest {
     }
 
     @Test
-    @DisplayName("정상적인 최신 백업 기록을 요청한 경우 백업한 기록이 존재하면 백업 기록을 출력해준다...")
+    @DisplayName("정상적인 최신 백업 기록을 요청한 경우 백업한 기록이 존재하면 백업 기록을 출력해준다.")
     public void getRecentBackUpAPITest() throws Exception {
         //given
         BackUp backUp = BackUp.of("asdasd","os1","device1",3,avatar);
@@ -86,6 +89,7 @@ class BackUpControllerTest {
                 .session(httpSession)
                 .contentType(MediaType.APPLICATION_JSON)
         )
+                .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.backUpID").exists())
                 .andExpect(jsonPath("$.osVersion").exists())
@@ -95,7 +99,7 @@ class BackUpControllerTest {
     }
 
     @Test
-    @DisplayName("정상적인 최신 백업 기록을 요청한 경우 백업기록이 존재하지 않는 경우 에러메세지를 출력해준다...")
+    @DisplayName("정상적인 최신 백업 기록을 요청한 경우 백업기록이 존재하지 않는 경우 에러메세지를 출력해준다.")
     public void getRecentBackUpNullAPITest() throws Exception {
         //given
         //when

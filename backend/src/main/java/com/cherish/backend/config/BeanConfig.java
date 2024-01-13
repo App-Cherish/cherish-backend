05d74408a029480d81c349f4f379a5299da0716b
young ho moon <ansdudgh98@naver.com>
2024-01-13 17:37:50 +0900

    feat : "카카오 API 서버에 ID값 호출하는 기능 추가"#30

--- backend/src/main/java/com/cherish/backend/config/BeanConfig.java
+++ backend/src/main/java/com/cherish/backend/config/BeanConfig.java
@@ -2,14 +2,20 @@ package com.cherish.backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 import java.time.Clock;
 
 @Configuration
-public class ClockBeanConfig {
+public class BeanConfig {
 
     @Bean
     public Clock clock () {
         return Clock.systemDefaultZone();
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
---
+++ backend/src/main/java/com/cherish/backend/util/SocialLoginValidationUtil.java
@@ -0,0 +1,61 @@
+package com.cherish.backend.util;
+
+import com.cherish.backend.domain.Platform;
+import com.cherish.backend.exception.WrongOauthIdException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class SocialLoginValidationUtil {
+
+    private final RestTemplate restTemplate;
+
+    public void validation(String code, String accessToken, String platform) {
+        if (platform.equals(Platform.KAKAO.getValue())) {
+            kakaoLoginValidation(code, accessToken);
+        }
+        if (platform.equals(Platform.APPLE.getValue())) {
+            appleLoginValidation(code, accessToken);
+        }
+    }
+
+
+    private void kakaoLoginValidation(String code, String accessToken) {
+        String url = "https://kapi.kakao.com/v1/user/access_token_info";
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + accessToken);
+
+
+        ResponseEntity<KakaoValidationResponseDTO> response = restTemplate.exchange(
+                url, HttpMethod.GET, new HttpEntity(headers), KakaoValidationResponseDTO.class
+        );
+
+        if (!response.getBody().getId().equals(code)) {
+            throw new WrongOauthIdException();
+        }
+    }
+
+    private void appleLoginValidation(String code,String accessToken) {
+
+    }
+
+
+    static class KakaoValidationResponseDTO {
+        private String id;
+        private String expires_in;
+        private String app_id;
+
+        public String getId() {
+            return id;
+        }
+    }
+
+}

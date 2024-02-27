33bef527ded531048a6646c767fe233b3a85c337
young ho moon <ansdudgh98@naver.com>
2024-02-28 02:53:26 +0900

    feat : "일기 엔티티 관련 레파지토리 함수 작성 및 테스트 작성" #38

--- backend/src/main/java/com/cherish/backend/repositroy/DiaryRepositoryCustom.java
+++ backend/src/main/java/com/cherish/backend/repositroy/DiaryRepositoryCustom.java
@@ -1,12 +1,21 @@
 package com.cherish.backend.repositroy;
 
+import com.cherish.backend.domain.BackUp;
 import com.cherish.backend.domain.Diary;
 
 import java.util.List;
 
 public interface DiaryRepositoryCustom {
 
-    Diary findDiaryByIdAndAvatarId(Long diaryId, Long avatarId);
+    List<Diary> findDiariesByIdAndAvatarIdAndBackUpId(String backUpId, Long avatarId);
 
-    List<Diary> findDiariesByIdAndAvatarIdAndBackUpId(String backUpId ,Long avatarId);
+    void updateDiary(Diary newDiary);
+
+    void updateDiaryList(List<Diary> editDirayList);
+
+    void deActiveDiary(Diary newDiary);
+
+    void deActiveDiaryList(List<Diary> deleteDiaryList);
+
+    void updateBackUp(BackUp updateBackUp, Long diaryId);
 }
--- backend/src/test/java/com/cherish/backend/repositroy/DiaryRepositoryTest.java
+++ backend/src/test/java/com/cherish/backend/repositroy/DiaryRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.cherish.backend.repositroy;
 
 import com.cherish.backend.domain.*;
+import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -20,10 +22,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 class DiaryRepositoryTest {
 
     @Autowired
-    DiaryRepository diaryRepository;
+    EntityManager em;
 
     @Autowired
-    AvatarRepository avatarRepository;
+    DiaryRepository diaryRepository;
 
     @Autowired
     BackUpRepository backUpRepository;
@@ -32,35 +34,95 @@ class DiaryRepositoryTest {
 
     BackUp backUp;
 
+    Diary diary1, diary2, diary3;
+
+
     @BeforeEach
     public void init() {
         avatar = Avatar.of("name1", LocalDate.now(), Gender.MALE);
-        backUp = BackUp.of("os1","device1",3,avatar);
+        backUp = BackUp.of("os1", "device1", avatar);
 
-        avatarRepository.save(avatar);
-        backUpRepository.save(backUp);
+        em.persist(avatar);
+        em.persist(backUp);
+
+        diary1 = Diary.of(DiaryKind.FREE, "title1", "content1", LocalDateTime.now(), avatar, backUp);
+        diary2 = Diary.of(DiaryKind.FREE, "title2", "content2", LocalDateTime.now(), avatar, backUp);
+        diary3 = Diary.of(DiaryKind.FREE, "title3", "content3", LocalDateTime.now(), avatar, backUp);
+
+
+        em.persist(diary1);
+        em.persist(diary2);
+        em.persist(diary3);
     }
 
     @Test
     @DisplayName("여려개의 Diary entity가 데이터베이스에 저장되어있을 때 아바타 엔티티와 아이디와 백업 엔티티의 아이디로 조회가 가능해야 한다.")
     public void findDiariesByIdAndAvatarIdAndBackUpIdTest() throws Exception {
         //given
-        Diary diary1 = Diary.of(DiaryKind.FREE, "title1", "content1", LocalDateTime.now(), "device1", "deviceid1", avatar, backUp);
-        Diary diary2 = Diary.of(DiaryKind.FREE, "title2", "content2", LocalDateTime.now(), "device2", "deviceid2", avatar, backUp);
-        Diary diary3 = Diary.of(DiaryKind.FREE, "title3", "content3", LocalDateTime.now(), "device3", "deviceid3", avatar, backUp);
 
-        diaryRepository.save(diary1);
-        diaryRepository.save(diary2);
-        diaryRepository.save(diary3);
         //when
 
         List<Diary> findDiaryList = diaryRepository.findDiariesByIdAndAvatarIdAndBackUpId(backUp.getId(), avatar.getId());
         //then
 
         assertThat(findDiaryList.size()).isEqualTo(3);
-        assertThat(findDiaryList).extracting("id",String.class).contains(diary1.getId(),diary2.getId(),diary3.getId());
+        assertThat(findDiaryList).extracting("id", Long.class).contains(diary1.getId(), diary2.getId(), diary3.getId());
     }
 
+    @Test
+    @DisplayName("업데이트 시에 요쳥된 값에 대해서 변경이 이루어져야 하며 백업 값이 갱신 되어야 한다.")
+    public void updateTest() throws Exception {
+        BackUp newBackUp = BackUp.of(backUp.getOsVersion(), backUp.getDeviceType(), avatar);
+        em.persist(newBackUp);
+        em.flush();
+        //when
+        Diary findDiary = em.find(Diary.class, diary1.getId());
+        Diary updateDiary = Diary.of(findDiary.getId(), DiaryKind.QUESTION, "editTitle", "editContent", diary1.getClientWritingDate(), diary1.getAvatar(), newBackUp);
+        diaryRepository.updateDiary(updateDiary);
+        //then
+        em.flush();
+        em.clear();
+        Diary result = em.find(Diary.class, updateDiary.getId());
+        assertThat(result.getId()).isEqualTo(updateDiary.getId());
+        assertThat(result.getTitle()).isEqualTo(updateDiary.getTitle());
+        assertThat(result.getContent()).isEqualTo(updateDiary.getContent());
+    }
+
+    @Test
+    @DisplayName("한개의 일기에 대해서 비활성화 요청이 들어온 경우 해당 일기값 들이 비활성화 되어야 한다.")
+    public void deActiveTest() throws Exception {
+        //given
+        //when
+        diaryRepository.deActiveDiary(diary1);
+        //then
+        em.flush();
+        em.clear();
+        Diary expect = em.find(Diary.class, diary1.getId());
+        assertThat(expect.getActive()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("여러개의 일기에 대해서 비활성화 요청이 들어온 경우 해당 일기값 들이 비활성화 되어야 한다.")
+    public void deActiveListTest() throws Exception {
+        //given
+        //when
+        List<Diary> list = new ArrayList<>();
+        list.add(diary1);
+        list.add(diary2);
+        list.add(diary3);
+
+        diaryRepository.deActiveDiaryList(list);
+        //then
+        em.flush();
+        em.clear();
+        Diary findDiary1 = em.find(Diary.class, diary1.getId());
+        Diary findDiary2 = em.find(Diary.class, diary2.getId());
+        Diary findDiary3 = em.find(Diary.class, diary3.getId());
+
+        assertThat(findDiary1.getActive()).isEqualTo(0);
+        assertThat(findDiary2.getActive()).isEqualTo(0);
+        assertThat(findDiary3.getActive()).isEqualTo(0);
+    }
 
 
 }

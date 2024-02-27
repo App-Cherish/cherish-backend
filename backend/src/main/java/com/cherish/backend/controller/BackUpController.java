549802041835e1054d441c8ac775f9dec1f3705d
young ho moon <ansdudgh98@naver.com>
2024-02-28 02:51:13 +0900

    feat : "백업 API 컨트롤러 함수 작성 및 기존 백업 API 함수 삭제" #38

--- backend/src/main/java/com/cherish/backend/controller/BackUpController.java
+++ backend/src/main/java/com/cherish/backend/controller/BackUpController.java
@@ -1,23 +1,27 @@
 package com.cherish.backend.controller;
 
 import com.cherish.backend.controller.argumentresolver.LoginAvatarId;
-import com.cherish.backend.controller.dto.response.BackUpHistoryResponse;
-import com.cherish.backend.domain.BackUp;
+import com.cherish.backend.controller.dto.request.DiaryEventRequestList;
 import com.cherish.backend.service.BackUpService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/backup")
 @RequiredArgsConstructor
+@Slf4j
 public class BackUpController {
 
     private final BackUpService backUpService;
 
-    @GetMapping
-    public BackUpHistoryResponse loadRecentBackUpHistory(@LoginAvatarId Long avatarId) {
-        return backUpService.getRecentBackUp(avatarId);
+    @PostMapping
+    public void backUpDiary(@RequestBody DiaryEventRequestList diaryEventRequest, @LoginAvatarId Long avatarId) {
+        backUpService.backup(diaryEventRequest, avatarId);
     }
+
+
 }

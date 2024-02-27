3030dadf9636e852037516a802e7addcdebee760
young ho moon <ansdudgh98@naver.com>
2024-02-28 02:52:07 +0900

    feat : "백업 엔티티 비활성화 함수 추가" #38

--- backend/src/main/java/com/cherish/backend/repositroy/BackUpRepository.java
+++ backend/src/main/java/com/cherish/backend/repositroy/BackUpRepository.java
@@ -2,6 +2,7 @@ package com.cherish.backend.repositroy;
 
 import com.cherish.backend.domain.BackUp;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -17,5 +18,9 @@ public interface BackUpRepository extends JpaRepository<BackUp, String> {
 
     @Query("select count (b) > 0 from BackUp b where b.avatar.id = :avatarId and b.active = 1 ")
     Boolean ExistBackUpByAvatarId(@Param("avatarId") Long avatarId);
+
+    @Modifying
+    @Query("update BackUp b set b.active = 0 where b.id = :backUpId")
+    void deActiveBackUp(@Param("backUpId") String backUpId);
 
 }

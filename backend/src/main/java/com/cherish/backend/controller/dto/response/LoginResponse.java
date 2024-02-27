40e8a0c7dad627d736f14feb1a26e5a7a915ed7e
young ho moon <ansdudgh98@naver.com>
2024-02-28 02:44:31 +0900

    refactor : "요구사항 변경으로 인한 응답값 변경"

--- backend/src/main/java/com/cherish/backend/controller/dto/response/LoginResponse.java
+++ backend/src/main/java/com/cherish/backend/controller/dto/response/LoginResponse.java
@@ -1,6 +1,5 @@
 package com.cherish.backend.controller.dto.response;
 
-import com.cherish.backend.util.DateFormattingUtil;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
@@ -11,10 +10,8 @@ public class LoginResponse {
 
     private String tokenId;
 
-    private String expiredTime;
 
     public LoginResponse(String tokenId, LocalDateTime expiredTime) {
         this.tokenId = tokenId;
-        this.expiredTime = DateFormattingUtil.localDateTimeToString(expiredTime);
     }
 }

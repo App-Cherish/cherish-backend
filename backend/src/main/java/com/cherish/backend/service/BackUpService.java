ea1079b25c314ffcd1aa470fa1adbd4eae0df126
young ho moon <ansdudgh98@naver.com>
2024-03-19 12:13:03 +0900

    feat : "backup service 작성 및 일기 저장 및 불러오기, 최신 일기 가져오리 로직 작성" #38

--- backend/src/main/java/com/cherish/backend/service/BackUpService.java
+++ backend/src/main/java/com/cherish/backend/service/BackUpService.java
@@ -2,11 +2,15 @@ package com.cherish.backend.service;
 
 import com.cherish.backend.controller.dto.request.DiaryEventRequest;
 import com.cherish.backend.controller.dto.request.DiaryEventRequestList;
+import com.cherish.backend.controller.dto.response.BackUpHistoryResponse;
+import com.cherish.backend.controller.dto.response.DiaryResponse;
+import com.cherish.backend.controller.dto.response.RestoreDiaryResponse;
 import com.cherish.backend.domain.Avatar;
 import com.cherish.backend.domain.BackUp;
 import com.cherish.backend.domain.Diary;
 import com.cherish.backend.domain.DiaryEvent;
 import com.cherish.backend.exception.NotExistAvatarException;
+import com.cherish.backend.exception.NotExistBackUpHistoryException;
 import com.cherish.backend.repositroy.AvatarRepository;
 import com.cherish.backend.repositroy.BackUpRepository;
 import com.cherish.backend.repositroy.DiaryEventRepository;
@@ -16,7 +20,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -33,20 +36,22 @@ public class BackUpService {
     private final DiaryRepository diaryRepository;
 
     @Transactional
-    public void backup(DiaryEventRequestList requestList, Long avatarId) {
+    public BackUpHistoryResponse backup(DiaryEventRequestList requestList, Long avatarId) {
         Optional<BackUp> findBackUp = backUpRepository.findBackUpByIdLatest(avatarId);
         Avatar findAvatar = avatarRepository.findAvatarById(avatarId).orElseThrow(NotExistAvatarException::new);
-        BackUp backUp = backUpRepository.save(BackUp.of(requestList.getOsVersion(), requestList.getDeviceType(), findAvatar));
+        BackUp backUp = BackUp.of(requestList.getOsVersion(), requestList.getDeviceType(), findAvatar);
 
         List<DiaryEvent> createDiaryEventList = requestList.toCreateDiaryEventEntity(findAvatar, backUp);
-        diaryEventRepository.saveAll(createDiaryEventList);
 
-        Map<LocalDateTime, DiaryEventRequest> editLastEventMap = requestList.toEditEventMapByLastEventDate();
+        Map<String, DiaryEventRequest> editLastEventMap = requestList.toEditEventMapByLastEventDate();
         List<Diary> diaryList = requestList.toCreateDiaryEntityModifiedByEditEventList(editLastEventMap, findAvatar, backUp);
 
         diaryList = requestList.createDiaryListDeleteByDeleteEventList(diaryList);
 
+        Integer count = diaryList.size();
+
         diaryRepository.saveAll(diaryList);
+
         if (findBackUp.isPresent()) {
             List<Diary> findDiaryList = diaryRepository.findDiariesByIdAndAvatarIdAndBackUpId(findBackUp.get().getId(), avatarId);
 
@@ -56,16 +61,42 @@ public class BackUpService {
 
             List<Diary> deleteDiaryList = requestList.toDeleteDiaryListFromFindDiaryList(findDiaryList, findAvatar);
 
+            count += editDiaryList.size();
+
             backUpRepository.deActiveBackUp(findBackUp.get().getId());
             diaryRepository.updateDiaryList(editDiaryList);
             diaryRepository.deActiveDiaryList(deleteDiaryList);
         }
+        backUp.setCount(count);
+        backUpRepository.save(backUp);
+        diaryEventRepository.saveAll(createDiaryEventList);
 
         List<DiaryEvent> diaryEditEvents = requestList.toEditDiaryEventEntity(findAvatar, backUp);
         List<DiaryEvent> diaryDeleteEvents = requestList.toDeleteDiaryEventEntity(findAvatar, backUp);
         diaryEventRepository.saveAll(diaryEditEvents);
         diaryEventRepository.saveAll(diaryDeleteEvents);
 
+        return new BackUpHistoryResponse(backUp.getId(), backUp.getCreatedDate(), backUp.getDeviceType(), backUp.getOsVersion(), backUp.getCount());
+    }
+
+    public BackUpHistoryResponse getBackUp(Long avatarId) {
+        return backUpRepository.findBackUpByIdLatest(avatarId).orElseThrow(NotExistBackUpHistoryException::new).toDto();
+    }
+
+    public RestoreDiaryResponse restoreDiary(Long avatarId, String backUpId) {
+        List<Diary> diaryList = diaryRepository.findDiariesByIdAndAvatarIdAndBackUpId(backUpId, avatarId);
+
+        if (diaryList.isEmpty()) {
+            throw new NotExistBackUpHistoryException();
+        }
+
+        List<DiaryResponse> diaryResponse = diaryList.stream()
+                .map(d -> new DiaryResponse(d.getClientId(), d.getTitle(), d.getContent(), d.getKind(), d.getClientWritingDate()))
+                .toList();
+
+        BackUp backUp = diaryList.get(0).getBackUp();
+
+        return new RestoreDiaryResponse(diaryResponse, backUp.getId(), backUp.getCreatedDate(), backUp.getCount(), backUp.getDeviceType(), backUp.getOsVersion());
     }
 
 }

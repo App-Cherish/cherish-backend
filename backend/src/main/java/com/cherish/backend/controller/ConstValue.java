ccd00a9c1669521286a9415f22227941d7d1b34a
young ho moon <ansdudgh98@naver.com>
2024-03-24 20:14:58 +0900

    remove : "필요없는 변수 삭제"

--- backend/src/main/java/com/cherish/backend/controller/ConstValue.java
+++ backend/src/main/java/com/cherish/backend/controller/ConstValue.java
@@ -2,7 +2,6 @@ package com.cherish.backend.controller;
 
 public interface ConstValue {
 
-    final static String tokenCookieName = "sessiontoken";
     final static String sessionName = "avatarId";
 
 }

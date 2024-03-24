1c8adbddebfb2273334a638dbcb1efb093961fdd
young ho moon <ansdudgh98@naver.com>
2024-03-24 14:36:46 +0900

    refactor : "주석해제"

--- backend/src/main/java/com/cherish/backend/config/HibernateConfig.java
+++ backend/src/main/java/com/cherish/backend/config/HibernateConfig.java
@@ -1,18 +1,22 @@
 package com.cherish.backend.config;
 
+import com.cherish.backend.util.QueryCountInspector;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.cfg.AvailableSettings;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernatePropertiesCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
 public class HibernateConfig {
 
-//    private final QueryCountInspector queryCountInspector;
-//
-//    @Bean
-//    public HibernatePropertiesCustomizer configureStatementInspector() {
-//        return hibernateProperties ->
-//                hibernateProperties.put(AvailableSettings.STATEMENT_INSPECTOR, queryCountInspector);
-//    }
+    private final QueryCountInspector queryCountInspector;
+
+    @Bean
+    public HibernatePropertiesCustomizer configureStatementInspector() {
+        return hibernateProperties ->
+                hibernateProperties.put(AvailableSettings.STATEMENT_INSPECTOR, queryCountInspector);
+    }
 
 }
